af8aee0358e94f39a69d451c8adf4d49f7b6b852
Mandal <S.Mandal@zbw-nett.zbw-kiel.de>
2022-07-04 14:35:22 +0200

    Updates

---
+++ precache-manifest.eeb5b37611f4cc3b2c5c6d622e8d4791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa854fd4524f66740f9a6075474b1be",
+    "url": "/demoapp/index.html"
+  },
+  {
+    "revision": "c28831eea4c19bd4eae5",
+    "url": "/demoapp/static/css/main.ae8ec85a.chunk.css"
+  },
+  {
+    "revision": "508b5e7ada8e86353076",
+    "url": "/demoapp/static/js/2.7a0f2d9d.chunk.js"
+  },
+  {
+    "revision": "c28831eea4c19bd4eae5",
+    "url": "/demoapp/static/js/main.0c146683.chunk.js"
+  },
+  {
+    "revision": "c2686fe6235231940a7f",
+    "url": "/demoapp/static/js/runtime~main.55d7d72e.js"
+  },
+  {
+    "revision": "21cf2782fd717ab683f459e7342e3bcc",
+    "url": "/demoapp/static/media/spinner.21cf2782.gif"
+  }
+]);
